c2ec4912bbab469274c4c15abffa33a4a5fdea41
Pablo <pmunoz.repo@gmail.com>
2024-09-13 00:26:01 -0300

    "fix"

--- stockTracker.py
+++ stockTracker.py
@@ -3,45 +3,6 @@ import numpy as np
 import datetime
 import time
 import matplotlib.pyplot as plt
-import RPi.GPIO as GPIO
-
-# for each of the emas (slow, mid, fast) we have a green-yellow-red led light
-GPIO.setmode(GPIO.BCM)
-pins = {
-    'slow': {
-        'red': 27,
-        'yellow': 18,
-        'green': 17
-    },  
-    'mid': {
-        'red': 22,
-        'yellow': 23,
-        'green': 24
-    },
-    'fast': {
-        'red': 20,
-        'yellow': 26,
-        'green': 21
-    }
-}
-for ema, colors in pins.items():
-    for color, pin in colors.items():
-        GPIO.setup(pin, GPIO.OUT)
-        
-
-def turn_on_led(pin):
-    GPIO.output(pin, GPIO.HIGH)
-
-def turn_off_led(pin):
-    GPIO.output(pin, GPIO.LOW)
-
-def flash_led(pin, times):
-    for _ in range(times):
-        turn_off_led(pin)
-        turn_on_led(pin)
-        time.sleep(0.5)
-        turn_off_led(pin)
-        time.sleep(0.5)
 
 def get_bitcoin_price():
     url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
@@ -49,23 +10,6 @@ def get_bitcoin_price():
     data = response.json()
     price = data['bpi']['USD']['rate_float']
     return price
-
-def ema_indicator(price, ema, pins, flash=0):
-    # if the price is near to the EMA, turn on yellow LED. If the price is above the EMA, turn on green LED. If the price is below the EMA, turn on red LED.
-    # use a level of tolerance for the price to be considered near the EMA
-    if ema is None:
-        return
-        
-    tolerance = 0.1
-    if flash:
-        flash_led(pins['red'], flash)
-
-    if price < ema * (1 - tolerance):
-        turn_on_led(pins['red'])
-    elif price > ema * (1 + tolerance):
-        turn_on_led(pins['green'])
-    else:
-        turn_on_led(pins['yellow'])
 
 def calculate_ema(prices, window):
     if(len(prices) < window):
@@ -85,6 +29,74 @@ def calculate_emas(bitcoin_prices):
     fast_ema = calculate_ema(bitcoin_prices, 5)
     return slow_ema, mid_ema, fast_ema
 
+class LED:
+        def __init__(self, pin):
+            self.pin = pin
+            
+        def flash(self, times, speed):
+            print(f'Flashing LED on pin {self.pin} {times} times at speed {speed}')
+
+class SemaphoreLED:
+    def __init__(self, red_pin, yellow_pin, green_pin):
+        self.leds = {
+            'red': LED(red_pin),
+            'yellow': LED(yellow_pin),
+            'green': LED(green_pin)
+        }
+    
+# Object that has a handle for a SemaphoreLED (mock for now)
+# and turns on the lights based on the Price to EMA ratio
+class EMA_Indicator:
+    def __init__(self):
+        self.lights = {
+            "fast": SemaphoreLED(22, 24, 27),
+            "mid": SemaphoreLED(23, 25, 28),
+            "slow": SemaphoreLED(26, 29, 30)
+        }
+
+    def update(self, price, slow_ema, mid_ema, fast_ema):
+        if slow_ema is not None:
+            slow_ema_ratio = (price / slow_ema - 1.0) * 100.0
+            if slow_ema_ratio < 0:
+                print(f'\033[91mSlow EMA ratio: {slow_ema_ratio}\033[0m')
+            else:
+                print(f'\033[92mSlow EMA ratio: {slow_ema_ratio}\033[0m')
+
+            if slow_ema_ratio < 0:
+                self.lights["slow"].leds["red"].flash(3, 2)
+            elif slow_ema_ratio == 0:
+                self.lights["slow"].leds["yellow"].flash(3, 2)
+            else:
+                self.lights["slow"].leds["green"].flash(3, 2)
+
+        if mid_ema is not None:
+            mid_ema_ratio = (price / mid_ema - 1.0) * 100.0
+            if mid_ema_ratio < 0:
+                print(f'\033[91mMid EMA ratio: {mid_ema_ratio}\033[0m')
+            else:
+                print(f'\033[92mMid EMA ratio: {mid_ema_ratio}\033[0m')
+
+            if mid_ema_ratio < 0:
+                self.lights["mid"].leds["red"].flash(3, 2)
+            elif mid_ema_ratio == 0:
+                self.lights["mid"].leds["yellow"].flash(3, 2)
+            else:
+                self.lights["mid"].leds["green"].flash(3, 2)
+        
+        if fast_ema is not None:
+            fast_ema_ratio = (price / fast_ema - 1.0) * 100.0
+            if fast_ema_ratio < 0:
+                print(f'\033[91mFast EMA ratio: {fast_ema_ratio}\033[0m')
+            else:
+                print(f'\033[92mFast EMA ratio: {fast_ema_ratio}\033[0m')
+
+            if fast_ema_ratio < 0:
+                self.lights["fast"].leds["red"].flash(3, 2)
+            elif fast_ema_ratio == 0:
+                self.lights["fast"].leds["yellow"].flash(3, 2)
+            else:
+                self.lights["fast"].leds["green"].flash(3, 2)
+
 bitcoin_prices = []
 timestamps = []
 slow_emas = []
@@ -94,6 +106,8 @@ fast_emas = []
 plt.ion()  # Turn on interactive mode for real-time plotting
 
 fig, ax = plt.subplots()
+
+indicator = EMA_Indicator()
 
 while True:
     price = get_bitcoin_price()
@@ -117,32 +131,6 @@ while True:
     plt.draw()  # Redraw the plot
     plt.pause(0.1)  # Pause to allow the plot to update
 
-    if slow_ema[-1] is not None:
-        slow_ema_ratio = (price / slow_ema[-1] - 1.0) * 100.0
-        if slow_ema_ratio < 0:
-            print(f'\033[91mSlow EMA ratio: {slow_ema_ratio}\033[0m')
-        else:
-            print(f'\033[92mSlow EMA ratio: {slow_ema_ratio}\033[0m')
-
-    if mid_ema[-1] is not None:
-        mid_ema_ratio = (price / mid_ema[-1] - 1.0) * 10000.0
-        if mid_ema_ratio < 0:
-            print(f'\033[91mMid EMA ratio: {mid_ema_ratio}\033[0m')
-        else:
-            print(f'\033[92mMid EMA ratio: {mid_ema_ratio}\033[0m')
-
-    if fast_ema[-1] is not None:
-        fast_ema_ratio = (price / fast_ema[-1] - 1.0) * 10000.0
-        if fast_ema_ratio < 0:
-            print(f'\033[91mFast EMA ratio: {fast_ema_ratio}\033[0m')
-        else:
-            print(f'\033[92mFast EMA ratio: {fast_ema_ratio}\033[0m')
-
-    print("")
-
-    # Use led flash indicators with the last prices and emas
-    ema_indicator(price, slow_ema[-1], pins['slow'], flash=1)
-    ema_indicator(price, mid_ema[-1], pins['mid'], flash=2)
-    ema_indicator(price, fast_ema[-1], pins['fast'], flash=3)
+    indicator.update(price, slow_ema[-1], mid_ema[-1], fast_ema[-1])
 
     time.sleep(15)
