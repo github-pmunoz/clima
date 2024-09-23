6a3abec69386d739cc2452c12ec28db3c4f4dc77
Pablo <pmunoz.repo@gmail.com>
2024-09-22 22:34:58 -0300

    .

--- checkWeatherDatabase.py
+++ checkWeatherDatabase.py
@@ -160,14 +160,12 @@ axs[1, 1].axhline(0, color='0.5', linestyle='--')
 axs[1, 1].fill_between(derivative_timestamps, derivative, 0, where=derivative>0, color='red', alpha=0.5)
 axs[1, 1].fill_between(derivative_timestamps, derivative, 0, where=derivative<0, color='cyan', alpha=0.5)
 #add vertical dashed-dotted lines when the derivative changes sign
+#after a change of sign, the next one needs to be at least 1 hour later
 for i in range(1, len(derivative)):
-    if derivative[i] * derivative[i-1] < 0:
+    if derivative[i] * derivative[i-1] < 0 and derivative_timestamps[i] - derivative_timestamps[i-1] > 60*60:
         axs[1, 1].axvline(derivative_timestamps[i], color='0.5', linestyle='-.')
-#add the date on top of the vetical line
-for i in range(1, len(derivative)):
-    if derivative[i] * derivative[i-1] < 0:
         axs[1, 1].text(derivative_timestamps[i], max(derivative), datetime.datetime.fromtimestamp(derivative_timestamps[i]).strftime('%m-%d'), verticalalignment='bottom', horizontalalignment='center', color='0.5')
-
+        
 
 # Adjust the layout
 plt.tight_layout()
