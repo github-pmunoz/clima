8862cb071615883c3b56f9af866d9f88a53f0f65
Pablo <pmunoz.repo@gmail.com>
2024-09-22 22:43:28 -0300

    .

--- checkWeatherDatabase.py
+++ checkWeatherDatabase.py
@@ -159,7 +159,7 @@ axs[1, 1].fill_between(derivative_timestamps, derivative, 0, where=derivative<0,
 #after a change of sign, the next one needs to be at least 1 hour later
 for i in range(1, len(derivative)):
     last_sign_change = 0
-    if derivative[i] * derivative[i-1] < 0 and derivative_timestamps[i] - last_sign_change > 60*60(2):
+    if derivative[i] * derivative[i-1] < 0 and derivative_timestamps[i] - last_sign_change > 60*60*2:
         axs[1, 1].axvline(derivative_timestamps[i], color='0.5', linestyle='-.')
         axs[1, 1].text(derivative_timestamps[i], max(derivative), datetime.datetime.fromtimestamp(derivative_timestamps[i]).strftime('%H-%M'), verticalalignment='bottom', horizontalalignment='center', color='0.5')
         last_sign_change = derivative_timestamps[i]
