8c90ca102f5bfd49bdf1805e638a30e508725de1
Pablo <pmunoz.repo@gmail.com>
2024-09-24 00:41:57 -0300

    .

--- fixDB.py
+++ fixDB.py
@@ -5,10 +5,6 @@ import sys
 conn = sqlite3.connect(sys.argv[1])
 cursor = conn.cursor()
 
-# Add two columns to the existing table
-cursor.execute("ALTER TABLE measurements ADD COLUMN pressure REAL")
-cursor.execute("ALTER TABLE measurements ADD COLUMN sensor TEXT")
-
 # Update all existing rows with None values
 cursor.execute("UPDATE measurements SET pressure = NULL, sensor = DHT11")
 
